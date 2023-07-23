251726c4b1479ae62a68de43b8255023ca35dd8f
Zigguraptor <cobaltum8@gmail.com>
2023-07-23 18:40:04 +0400

    added alias

--- Attributes/OptionAttribute.cs
+++ Attributes/OptionAttribute.cs
@@ -7,6 +7,12 @@ public class OptionAttribute : ArgAttribute
         Name = name;
     }
 
+    public OptionAttribute(char alias, string name) : this(name)
+    {
+        Alias = alias;
+    }
+
+    public char Alias { get; set; }
     public string Name { get; set; }
     public string? GroupName { get; set; }
     public string? HelpText { get; set; }
