f727184650d6ec5769a2484323cafac8987801c5
Zigguraptor <cobaltum8@gmail.com>
2023-07-26 02:06:20 +0400

    refactored constructors

--- Attributes/OptionAttribute.cs
+++ Attributes/OptionAttribute.cs
@@ -2,9 +2,25 @@
 
 public class OptionAttribute : ArgAttribute
 {
+    public char? Alias { get; set; }
+    public string Name { get; set; }
+    public int? Min { get; set; }
+    public int? Max { get; set; }
+    public string HelpText { get; set; } = string.Empty;
+
     public OptionAttribute(string name)
     {
         Name = name;
+    }
+
+    public OptionAttribute(string name, int min) : this(name)
+    {
+        Min = min;
+    }
+
+    public OptionAttribute(string name, int min, int max) : this(name, min)
+    {
+        Max = max;
     }
 
     public OptionAttribute(char alias, string name) : this(name)
@@ -12,9 +28,13 @@ public class OptionAttribute : ArgAttribute
         Alias = alias;
     }
 
-    public char? Alias { get; set; }
-    public string Name { get; set; }
-    public int? Min { get; set; }
-    public int? Max { get; set; }
-    public string HelpText { get; set; } = string.Empty;
+    public OptionAttribute(char alias, string name, int min) : this(alias, name)
+    {
+        Min = min;
+    }
+
+    public OptionAttribute(char alias, string name, int min, int max) : this(alias, name, min)
+    {
+        Max = max;
+    }
 }
