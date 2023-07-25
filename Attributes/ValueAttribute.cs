5207f71ed39561e6e1108687e8e6c14265ebb5dc
Zigguraptor <cobaltum8@gmail.com>
2023-07-26 02:06:20 +0400

    refactored constructors

--- Attributes/ValueAttribute.cs
+++ Attributes/ValueAttribute.cs
@@ -6,4 +6,19 @@ public class ValueAttribute : ArgAttribute
     public int? Max { get; set; }
     public int? Min { get; set; }
     public string HelpText { get; set; } = string.Empty;
+
+    public ValueAttribute(int index)
+    {
+        Index = index;
+    }
+
+    public ValueAttribute(int index, int min) : this(index)
+    {
+        Min = min;
+    }
+
+    public ValueAttribute(int index, int min, int max) : this(index, min)
+    {
+        Max = max;
+    }
 }
