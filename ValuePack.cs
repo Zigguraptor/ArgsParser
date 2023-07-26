c7acab0ab98d15ba05003c59dec8df8c6c5d364d
Zigguraptor <cobaltum8@gmail.com>
2023-07-26 07:04:13 +0400

    TODO added

--- ValuePack.cs
+++ ValuePack.cs
@@ -6,7 +6,10 @@ namespace ArgsParser;
 public class ValuePack
 {
     public readonly PropertyInfo PropertyInfo;
+
     public readonly ValueAttribute ValueAttribute;
+
+    //TODO check this
     public readonly GroupAttribute? GroupAttribute;
 
     public ValuePack(PropertyInfo propertyInfo, ValueAttribute valueAttribute, GroupAttribute? groupAttribute)
